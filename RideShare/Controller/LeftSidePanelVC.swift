9a23d0ff923871f3f4c2ed301d95705a17e5d20f
Luis Calvillo <luiscalvillo@Luiss-MacBook-Pro.local>
2019-02-09 17:39:27 -0800

    Added LoginVC

--- RideShare/Controller/LeftSidePanelVC.swift
+++ RideShare/Controller/LeftSidePanelVC.swift
@@ -16,15 +16,12 @@ class LeftSidePanelVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUpLoginBtnWasPressed(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginVC") as? LoginVC
+        present(loginVC!, animated: true, completion: nil)
     }
-    */
+    
+ 
 
 }
