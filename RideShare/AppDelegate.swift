7a395ff29873c5f61244ffc3314b39095a51861f
Luis Calvillo <luiscalvillo@Luiss-MacBook-Pro.local>
2019-02-10 21:44:30 -0800

    Added DataService class

--- RideShare/AppDelegate.swift
+++ RideShare/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        FirebaseApp.configure()
         // The root view controller is set to the containerVC
         containerVC = ContainerVC()
         window?.rootViewController = containerVC
